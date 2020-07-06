d7b831205f5b57b19fececb9f89c9c126b4336e7
ASHIM KUMAR SHAHA <ashimkumar69@gmail.com>
2020-07-06 18:40:19 +0600

    login email verify done

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// auth
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth',
+    'namespace' => 'Backend\Auth',
+], function () {
+    Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
+    Route::get('user', 'AuthController@user');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('logout', 'AuthController@logout');
+    // auth email verify
+    Route::get('/email/resend', 'EmailVerificationController@resend')->name('verification.resend');
+    Route::get('/email/verify/{id}/{hash}', 'EmailVerificationController@verify')->name('verification.verify');
+});
